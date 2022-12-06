7725eeabd424ec6e4446776f8ecdbc9b8fc97e7e
sayanth A <sayanthsayu619@gmail.com>
2022-12-06 15:52:37 +0530

    sd

--- lib/app/home_view/view_model/home_controler.dart
+++ lib/app/home_view/view_model/home_controler.dart
@@ -1,29 +1,45 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:toast/toast.dart';
 
 class HomeController extends ChangeNotifier {
 //TOOGLE================================
+  HomeController() {
+    onScan();
+  }
+  final flutterBlueP = FlutterBluePlus.instance;
 
   bool value = false;
 
   onPressToggle(bool newValue) async {
     if (value) {
       value = newValue;
-      await FlutterBluePlus.instance.turnOff();
+      Platform.isAndroid ? await FlutterBluePlus.instance.turnOff() : null;
+
       notifyListeners();
     } else {
       value = newValue;
       await FlutterBluePlus.instance.turnOn();
+      Platform.isAndroid ? await FlutterBluePlus.instance.turnOn() : null;
       notifyListeners();
     }
   }
 
   //LIST OF DEVICE ===========================
-  var flutterBlueP = FlutterBluePlus.instance;
 
-  startScan() async {
+  onScan() {
     if (value == true) {
-      notifyListeners();
+      flutterBlueP.scanResults.listen((results) {
+        for (ScanResult r in results) {
+          print('${r.device.name} found! rssi: ${r.rssi}');
+        }
+      });
     }
+  }
+
+  void showToast(String msg, {int? duration, int? gravity}) {
+    Toast.show(msg, duration: duration, gravity: gravity);
   }
 }
